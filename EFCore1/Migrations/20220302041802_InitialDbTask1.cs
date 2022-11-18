a239663af3aa0f7ad99632b820fedb6a6705d2f1
Mohammed Cloudfreake <sedebay.kijani@gmail.com>
2022-11-18 11:50:25 -0500

    Add project files.

---
+++ EFCore1/Migrations/20220302041802_InitialDbTask1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore1.Migrations
+{
+    public partial class InitialDbTask1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    Did = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.Did);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Eid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salary = table.Column<double>(type: "float", nullable: false),
+                    DOB = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Did = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Eid);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Department");
+
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
---
+++ EFCore1/Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+//Console.WriteLine("Done!");
+
+[Table("Department")]
+public class Department
+{
+    [Key]
+    public int Did { get; set; }
+    public string? DName { get; set; }
+    public string? Description { get; set; }
+}
+
+[Table("Employee")]
+public class Employee
+{
+    [Key]
+    public int Eid { get; set; }
+    public string? EName { get; set; }
+    public string? Gender { get; set; }
+    public string? Email { get; set; }
+    public double Salary { get; set; }
+    public DateTime DOB { get; set; }
+    public int? Did { get; set; }
+}
+
+public class   OrganizationDbContext : DbContext
+{
+    #region Step - 4: Adding DbSets
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    #endregion
+
+    #region Step - 5: Configuring database connection
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer("Server=DESKTOP-DF2H2V7\\SQLEXPRESS2019;Database=OrgEFDbTask1;Trusted_Connection=True;");
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var D = new Department() { DName = "Finance", Description="Finance Department"};
+            
+            OrganizationDbContext dbContext = new OrganizationDbContext();
+            //dbContext.Departments.Add(D);
+            //dbContext.SaveChanges();
+
+            List<Department> departments = dbContext.Departments.ToList();
+            foreach (var d in departments)
+            {
+                Console.WriteLine($"Did:{d.Did} DName:{d.DName} Desc:{d.Description}");
+            }
+
+        }
+    }
+   
+}
+
+#endregion
